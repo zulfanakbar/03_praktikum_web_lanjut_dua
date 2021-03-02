ce96956d0b05941f894a4b2888be69120f216bda
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-02 14:21:19 +0700

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,16 @@
+<div class="row">
+	<div class="tm-header">
+		<a href="index.html" class="tm-site-name">Metro City</a>
+		<div class="mobile-menu-icon">
+	        <i class="fa fa-bars"></i>
+	    </div>
+		<nav class="tm-nav">
+            <ul>
+                <li><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/features') }}">Features</a></li>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+		</nav>
+	</div>			
+</div>	
